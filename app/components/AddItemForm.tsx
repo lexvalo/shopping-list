3fd1ca62a535ce97c00fdbbba7a5ddd2b5ab073d
lexvalo <aleksandr.valov@tutors.es>
2025-06-05 16:35:43 +0300

    refactor: move input form higher to avoid overlap with smartphone menu

--- app/components/AddItemForm.tsx
+++ app/components/AddItemForm.tsx
@@ -41,7 +41,7 @@ export default function AddItemForm({ onAdd }: Props) {
 
 const styles = StyleSheet.create({
     container: {
-        marginBottom: 24,
+        marginBottom: 92,
         marginLeft: 3,
         marginRight: 3,
         display: 'flex',
